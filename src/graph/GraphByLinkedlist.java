ba0e5ff7c34a33815473af23f789e471ed402f8e
TrinhLeKhac <emperorfire52@gmail.com>
2024-06-17 06:08:47 +0700

    Add Depth-frist search by recursive and method to find ConnectedComponent in graph

--- src/graph/GraphByLinkedlist.java
+++ src/graph/GraphByLinkedlist.java
@@ -1,5 +1,6 @@
 package graph;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -56,7 +57,7 @@ public class GraphByLinkedlist {
         }
     }
 
-    public void dfs (int s) {
+    public void dfsByStack(int s) {
         boolean[] isVisited = new boolean[this.V];
         Stack<Integer> stack = new Stack<>();
         stack.push(s);
@@ -73,6 +74,62 @@ public class GraphByLinkedlist {
         }
     }
 
+    public void dfsByRecursive(int s) {
+        boolean[] isVisited = new boolean[this.V];
+        dfsRecursive(s, isVisited);
+    }
+
+    public void dfsRecursive(int v, boolean[] isVisited) {
+        isVisited[v] = true;
+        System.out.print(v + " ");
+        // Reverse order of LinkedList
+        Collections.reverse(adjMatrix[v]);
+        for (int e: adjMatrix[v]) {
+            if (!isVisited[e]) {
+                dfsRecursive(e, isVisited);
+            }
+        }
+    }
+
+    // ConnectedComponent
+    // record is special kind of class introduced in Java 14 and made stable in Java 16
+    // It is used to create immutable data classes without having to write boilerplate code
+    // like getters, equals, hashCode and toString methods
+
+    public record ConnectedComponent(int count, int[] componentId) {
+        public int getCountCC() {
+            return count;
+        }
+
+        public boolean isConnected(int x, int y) {
+            return componentId[x]  == componentId[y];
+        }
+    }
+
+    public ConnectedComponent dfsConnectedComponent() {
+        boolean[] isVisited = new boolean[this.V];
+        int[] componentId = new int[this.V];  // vertex is belonged to which componentId
+        int count = 0;
+        for (int v = 0; v < V; v++) {
+            if (!isVisited[v]) {
+                dfsCC(v, isVisited, componentId, count);  // find all vertical that are belonged to the same component
+                count++;
+            }
+        }
+        return new ConnectedComponent(count, componentId);
+    }
+
+    public void dfsCC(int v, boolean[] isVisited, int[] componentId, int count) {
+        isVisited[v] = true;
+        componentId[v] = count;
+//        Collections.reverse(adjMatrix[v]);
+        for (int w: adjMatrix[v]) {
+            if (!isVisited[w]) {
+                dfsCC(w, isVisited, componentId, count);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         GraphByLinkedlist g = new GraphByLinkedlist(5);
         g.addEdges(0, 1);
@@ -81,15 +138,38 @@ public class GraphByLinkedlist {
         g.addEdges(3, 0);
         g.addEdges(2, 4);
 
-        System.out.println("----------------Adjacency matrix of graph-----------------------");
+        System.out.println("----------------Adjacency matrix of graph G1-----------------------");
         System.out.println(g);
 
         System.out.println("----------------Breath-first search (BFS)-----------------------");
         g.bfs(0);
         System.out.println('\n');
 
-        System.out.println("----------------Depth-first search (DFS)-----------------------");
-        g.dfs(0);
+        System.out.println("----------------Depth-first search (DFS) by stack-----------------------");
+        g.dfsByStack(0);
         System.out.println('\n');
+
+        System.out.println("----------------Depth-first search (DFS) by recursive-----------------------");
+        g.dfsByRecursive(0);
+        System.out.println('\n');
+
+        GraphByLinkedlist g2 = new GraphByLinkedlist(6);
+        g2.addEdges(0, 1);
+        g2.addEdges(0, 3);
+        g2.addEdges(2, 4);
+
+        System.out.println("----------------Adjacency matrix of graph G2-----------------------");
+        System.out.println(g2);
+
+        System.out.println("----------------Find ConnectedComponent-----------------------");
+        ConnectedComponent cc = g2.dfsConnectedComponent();
+        System.out.println("There are totally " + cc.getCountCC() + " connected-component in graph");
+        int vertex1 = 1;
+        int vertex2 = 2;
+        if (cc.isConnected(vertex1, vertex2)) {
+            System.out.println("The vertex " + vertex1 + " and " + vertex2 + " is connected");
+        } else {
+            System.out.println("The vertex " + vertex1 + " and " + vertex2 + " is NOT connected");
+        }
     }
 }
