df94f5841fa676024bb9591b7acdf4d18fc5c43f
TrinhLeKhac <emperorfire52@gmail.com>
2024-07-02 23:21:06 +0700

    Add method serialization

---
+++ src/stream/Serialization.java
@@ -0,0 +1,64 @@
+package stream;
+
+// Serializable is a marker interface (has no data member and method).
+// It is used to "mark" Java classes so that the objects of these classes may get a certain capability
+// The Serializable interface must be implemented by the class whose object needs to be persisted.
+// The String class and all the wrapper classes implement the java.io.Serializable interface by default.
+
+// The ObjectOutputStream class is used to write primitive data types, and Java objects to an OutputStream.
+// Only objects that support the java.io.Serializable interface can be written to streams.
+
+// An ObjectInputStream deserializes objects and primitive data written using an ObjectOutputStream
+
+import java.io.*;
+
+class Person implements Serializable {
+    int id;
+    String name;
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Student extends Person {
+    int fee;
+    String course;
+    public Student(int id, String name, String course, int fee) {
+        super(id, name);
+        this.course = course;
+        this.fee = fee;
+    }
+}
+
+public class Serialization {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        // Create object
+        Student std1 = new Student(211, "trinhlk2", "enginner", 50000);
+
+        // Creating stream and writing the object
+        // try-with-resources
+        try(
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src/stream/serialization.txt"));
+        ) {
+            out.writeObject(std1);
+            out.flush();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("Success");
+
+        // Creating stream to read the object
+        // try-with-resources
+        try(
+            ObjectInputStream in = new ObjectInputStream(new FileInputStream("src/stream/serialization.txt"));
+        ) {
+            Student s = (Student) in.readObject();
+            // Printing the data of the serialized object
+            System.out.println("(" + s.id + ", " + s.name + ", " + s.course + ", " + s.fee + ")");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
